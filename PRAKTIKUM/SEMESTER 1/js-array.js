9aa233bb09dfbc7a21a621616fb2878cd0d47236
indraprhmbd <indraprhmbd@gmail.com>
2025-02-28 18:13:01 +0700

    update file : StudiKasus3.cpp

--- PRAKTIKUM/SEMESTER 1/js-array.js
+++
@@ -1,6 +0,0 @@
-
-let a=[1,2,5,3,6,3,123,-2]
-
-let b="arsyadi"
-console.log(b.split(1))
-console.log(a.sort()) 
--- PRAKTIKUM/SEMESTER 2/123240062_Arsyadi_Indra_Hasan_StudiKasus3.cpp
+++ PRAKTIKUM/SEMESTER 2/123240062_Arsyadi_Indra_Hasan_StudiKasus3.cpp
@@ -27,21 +27,23 @@ int search_nama(string nama){
 }
 
 int search_nim(int nim){
-    mhs[N].nim=nim; //sentinel
+    mhs[N-1].nim=nim; //sentinel
     int i=0;
-    while(mhs[i].nim!=nim){
-        i++;
-    }
+    while(mhs[i].nim!=nim) i++;
     if(i<(N-1)) {cout<<"Ditemukan! Mahasiswa dengan nama : "<<mhs[i].nama<<endl;return 0;}
     else return -1;
 }
 
-void search_program_studi(string prog_studi){
+int search_prodi(string prog_studi){
+    int count=0;
     for(int i=0;i<N-1;i++){
         if(prog_studi==mhs[i].prog_studi){
             cout<<"Ditemukan! Mahasiswa dengan nama : "<<mhs[i].nama<<endl;
+            count++;
         }
     }
+    if(count==0) return -1;
+    else return 0;
 }
 
 int main(){
@@ -57,7 +59,6 @@ int main(){
         cout<<"pilih : ";cin>>menu;
         if(menu==1){
             string nama;
-            bool verify=false;
             cout<<"==Cari Berdasarkan Nama=="<<endl;
             cout<<"Masukkan Nama: ";cin.ignore();getline(cin,nama);
             if(search_nama(nama)==-1) cout<<"Data tidak ditemukan!"<<endl;
@@ -72,11 +73,10 @@ int main(){
             string prog_studi;
             cout<<"===Cari dengan program studi==="<<endl;
             cout<<"Masukkan program studi: ";cin.ignore();getline(cin,prog_studi);
-            search_program_studi(prog_studi);
+            if(search_prodi(prog_studi)==-1) cout<<"Data tidak ditemukan!"<<endl; 
             system("pause");
-        }else {
+        }else if(menu==0){
             cout<<"yakin ingin keluar? (y/t) : ";cin>>ulang;    
         }
-
     }while(ulang!='y');
 }
