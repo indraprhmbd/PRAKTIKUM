b1d0d2bab945367653ea28128c5f18717128d14b
indraprhmbd <indraprhmbd@gmail.com>
2025-02-28 19:04:46 +0700

    update file : StudiKasus3.cpp

--- PRAKTIKUM/SEMESTER 2/123240062_Arsyadi_Indra_Hasan_StudiKasus3.cpp
+++ PRAKTIKUM/SEMESTER 2/123240062_Arsyadi_Indra_Hasan_StudiKasus3.cpp
@@ -49,7 +49,7 @@ int search_prodi(string prog_studi){
 int main(){
     char ulang='t';
     do{
-        int menu;
+        char menu;
         system("cls");
         cout<<"===SI MAHASISWA==="<<endl;
         cout<<"1. Cari Berdasarkan Nama"<<endl;
@@ -57,25 +57,25 @@ int main(){
         cout<<"3. Cari Berdasarkan Program Studi (semua)"<<endl;
         cout<<"0. Keluar"<<endl;
         cout<<"pilih : ";cin>>menu;
-        if(menu==1){
+        if(menu=='1'){
             string nama;
             cout<<"==Cari Berdasarkan Nama=="<<endl;
             cout<<"Masukkan Nama: ";cin.ignore();getline(cin,nama);
             if(search_nama(nama)==-1) cout<<"Data tidak ditemukan!"<<endl;
             system("pause");
-        }else if(menu==2){
+        }else if(menu=='2'){
             int nim;
             cout<<"===Cari Dengan NIM==="<<endl;
             cout<<"Masukkan NIM : ";cin>>nim;
             if(search_nim(nim)==-1) cout<<"Data tidak ditemukan!"<<endl;
             system("pause");
-        }else if(menu==3){
+        }else if(menu=='3'){
             string prog_studi;
             cout<<"===Cari dengan program studi==="<<endl;
             cout<<"Masukkan program studi: ";cin.ignore();getline(cin,prog_studi);
             if(search_prodi(prog_studi)==-1) cout<<"Data tidak ditemukan!"<<endl; 
             system("pause");
-        }else if(menu==0){
+        }else if(menu=='0'){
             cout<<"yakin ingin keluar? (y/t) : ";cin>>ulang;    
         }
     }while(ulang!='y');
