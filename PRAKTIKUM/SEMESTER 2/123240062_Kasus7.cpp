bf3e9a17ad927600b2cad1c9fbd253d0000a9a75
indraprhmbd <indraprhmbd@gmail.com>
2025-05-12 07:55:05 +0700

    update file : Kasus7.cpp

--- PRAKTIKUM/SEMESTER 2/123240062_Kasus7.cpp
+++ PRAKTIKUM/SEMESTER 2/123240062_Kasus7.cpp
@@ -115,122 +115,113 @@ float generateRandomFloat(float min = 0.0f, float max = 100.0f) {
     return distrib(gen);
 }
 
-int main(){
-    char ulang='n';
-    int size=0;
-    do{
+void input_data(int& size) {
+    int qty;
+    char rand;
+    system("cls");
+    cout << "+================================+" << endl;
+    cout << "| [1] Input Data Mahasiswa       |" << endl;
+    cout << "+================================+" << endl;
+    cout << "Mau masukkan berapa data? : "; cin >> qty;
+    cout << "Random Data? (y/n) : "; cin >> rand;
+
+    if (size + qty > MAX_STUDENT) {
+        cout << "Data melebihi kapasitas maksimum!" << endl;
+        size = 0; // overwrite
+        return;
+    }
+
+    for (int i = size; i < qty + size; i++) {
+        if (rand == 'y') {
+            Data[i].nama = generateRandomString(generateRandomInt(4, 10));
+            Data[i].nilai = generateRandomFloat(min_nilai, max_nilai);
+        } else {
+            cin.ignore();
+            mhs* ptr = &Data[i];
+            cout << "[" << i + 1 << "]Masukkan Nama  : ";
+            getline(cin, ptr->nama);
+            if (ptr->nama.length() > MAX_NAME_CHAR) {
+                ptr->nama = ptr->nama.substr(0, MAX_NAME_CHAR);
+            }
+            cout << "   Masukkan Nilai : ";
+            cin >> ptr->nilai;
+            cout << string(40, '-') << endl;
+            if (ptr->nilai < min_nilai || ptr->nilai > max_nilai) {
+                i--; cout << "invalid value (min or max)" << endl;
+            }
+        }
+    }
+    size += qty;
+    cout << "Data telah masuk!" << endl;
+    pause();
+}
+
+void menu_tampilkan_data(int size) {
+    char ulang;
+    do {
+        system("cls");
+        int sortMenu;
+        if (size != 0) {
+            cout << "+================================+" << endl;
+            cout << "| [2] Tampilkan Data Mahasiswa   |" << endl;
+            cout << "+================================+" << endl;
+
+            sort_by_value(1, size); // default desc
+
+            cout << endl << "== Sorting Lain ==" << endl;
+            cout << "1. Nilai Ascending" << endl;
+            cout << "2. Nama Ascending (A-Z)" << endl;
+            cout << "3. Nama Descending (Z-A)" << endl;
+            cout << "4. Kembali" << endl;
+            cout << "Pilih : "; cin >> sortMenu;
+
+            auto start = chrono::high_resolution_clock::now();
+            this_thread::sleep_for(chrono::seconds(0));
+
+            if (sortMenu == 1) sort_by_value(0, size);
+            else if (sortMenu == 2 || sortMenu == 3) sort_by_name(sortMenu - 2, size);
+            else if (sortMenu == 4) return;
+            else cout << "Pilihan tidak valid!" << endl;
+
+            auto end = chrono::high_resolution_clock::now();
+            auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+            
+            cout << endl << "Elapsed time: " << duration.count() << " ms" << endl;
+            cout << endl << "Ulangi menu (y/n) : "; cin >> ulang;
+        } else {
+            cout << "Kosong! mohon isi data terlebih dahulu" << endl;
+            pause();
+            break;
+        }
+    } while (ulang != 'n');
+}
+
+int main() {
+    int size = 0;
+    char ulang = 'n';
+    do {
         system("cls");
         char menu;
-        cout<<"+================================+"<<endl;
-        cout<<"|       MENU DATA MAHASISWA      |"<<endl;
-        cout<<"+================================+"<<endl;
-        cout<<"| [1] Input Data Mahasiswa       |"<<endl;
-        cout<<"| [2] Tampilkan Data Mahasiswa   |"<<endl;
-        cout<<"| [3] Keluar                     |"<<endl;
-        cout<<"+================================+"<<endl;
-        cout<<"Pilih : "; cin>>menu;
+        cout << "+================================+" << endl;
+        cout << "|       MENU DATA MAHASISWA      |" << endl;
+        cout << "+================================+" << endl;
+        cout << "| [1] Input Data Mahasiswa       |" << endl;
+        cout << "| [2] Tampilkan Data Mahasiswa   |" << endl;
+        cout << "| [3] Keluar                     |" << endl;
+        cout << "+================================+" << endl;
+        cout << "Pilih : "; cin >> menu;
 
-        switch(menu){
-            case '1' :
-                int qty;
-                char rand;
-                system("cls");
-                cout<<"+================================+"<<endl;
-                cout<<"| [1] Input Data Mahasiswa       |"<<endl;
-                cout<<"+================================+"<<endl;
-                cout<<"Mau masukkan berapa data? : ";cin>>qty;
-                cout<<"Random Data? (y/n) : ";cin>>rand;
-                if (size + qty > MAX_STUDENT) {
-                    cout << "Data melebihi kapasitas maksimum!" << endl;
-                    size=0; //overwrite data dri awal
-                    break;
-                }
-                if (rand=='y'){
-                    for(int i=size;i<qty+size;i++){
-                        Data[i].nama = generateRandomString(generateRandomInt(4,10));
-                        Data[i].nilai = generateRandomFloat(min_nilai, max_nilai);
-                    }
-                }else{
-                    for(int i=size;i<qty+size;i++){
-                        cin.ignore();
-                        mhs* ptr = &Data[i];
-                        
-                        cout << "["<<i+1<<"]Masukkan Nama  : ";
-                        getline(cin, ptr->nama);
-
-                        //truncate jika lebih dari MAX_NAME_CHAR
-                        if(ptr->nama.length() > MAX_NAME_CHAR){
-                            ptr->nama = ptr->nama.substr(0, MAX_NAME_CHAR);
-                        }
-                        cout << "   Masukkan Nilai : ";
-                        cin >> ptr->nilai;
-                        cout<<string(40,'-')<<endl;
-                        if(ptr->nilai<min_nilai || ptr->nilai>max_nilai) {
-                            i--; cout<<"invalid value (min or max)"<<endl;
-                        }
-                    }
-                }
-                
-                size+=qty;
-                cout<<"Data telah masuk!"<<endl;
-                pause();
-                break;
-
-                case '2' :
-                do{
-                    system("cls");
-                    int sortMenu;
-                    if(size!=0){
-                        cout<<"+================================+"<<endl;
-                        cout<<"| [2] Tampilkan Data Mahasiswa   |"<<endl;
-                        cout<<"+================================+"<<endl;
-
-                        sort_by_value(1,size); //desc - default
-
-                        cout << endl << "== Sorting Lain ==" << endl;
-                        cout << "1. Nilai Ascending" << endl;
-                        cout << "2. Nama Ascending (A-Z)" << endl;
-                        cout << "3. Nama Descending (Z-A)" << endl;
-                        cout << "4. Kembali" << endl;
-                        cout << "Pilih : "; cin>>sortMenu;
-
-                        // Record the starting time point
-                        auto start = chrono::high_resolution_clock::now();
-                        // Code block to measure execution time
-                        this_thread::sleep_for(chrono::seconds(0));
-
-                        if (sortMenu == 1) {
-                            sort_by_value(0, size);
-                        } else if (sortMenu == 2 || sortMenu == 3) {
-                            sort_by_name(sortMenu - 2, size);
-                        } else if(sortMenu==4) {
-                            cout<<" "<<endl;
-                            ulang='n';
-                        }else {
-                            cout << "Pilihan tidak valid!" << endl;
-                        }
-                        
-                         // Record the ending time point
-                         auto end = chrono::high_resolution_clock::now();
-                         // Calculate the duration
-                         auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
-                         // Output the elapsed time
-                         cout <<endl << "Elapsed time: " << duration.count() << " ms" << std::endl;
-                        
-                    }else {cout<<"Kosong! mohon isi data terlebih dahulu"<<endl;
-                        pause();
-                        break;
-                    }
-                    if(sortMenu!=4) {cout<<endl<<"Ulangi menu (y/n) : ";cin>>ulang;}
-                }while(ulang!='n');
-                break;
-            }
-        ulang='n';//def 
-        if(menu=='3'){
-            cout<<"Yakin ingin keluar? (y/n) : ";cin>>ulang;
-            if(ulang=='y')
-                cout<<endl<<"T E R I M A K A S I H"<<endl<<" ";
+        switch (menu) {
+            case '1': input_data(size); break;
+            case '2': menu_tampilkan_data(size); break;
         }
-    }while(ulang!='y');
+
+        if (menu == '3') {
+            cout << "Yakin ingin keluar? (y/n) : "; cin >> ulang;
+            if (ulang == 'y')
+                cout << endl << "T E R I M A K A S I H" << endl;
+        }
+
+    } while (ulang != 'y');
     return 0;
 }
